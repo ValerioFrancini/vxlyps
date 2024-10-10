8bb792547adc5c1e53938115233ae748d6e0e3d7
Vxlyps <165580762+ValerioFrancini@users.noreply.github.com>
2024-10-10 17:51:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,3 @@ function toggleMenu() {
     const menu = document.getElementById('nav-menu');
     menu.classList.toggle('open');
 }
-
-// Slider - gestione della visualizzazione delle immagini
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slider-image');
-const totalImages = slides.length;
-
-function moveSlide(direction) {
-    slides[currentIndex].classList.remove('active'); // Nasconde l'immagine attuale
-    currentIndex = (currentIndex + direction + totalImages) % totalImages; // Calcola l'indice dell'immagine
-
