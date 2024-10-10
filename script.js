301b571c1cde735303316cbf6b23dd57e39f23de
Vxlyps <165580762+ValerioFrancini@users.noreply.github.com>
2024-10-10 17:35:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 /* script.js */
 
-function toggleMenu() {
-    const menu = document.getElementById('nav-menu');
-    menu.classList.toggle('open');
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slider-image');
+const totalImages = slides.length;
+
+function moveSlide(direction) {
+    slides[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + direction + totalImages) % totalImages;
+    slides[currentIndex].classList.add('active');
 }
+
+// Inizializza il primo slide come attivo
+slides[currentIndex].classList.add('active');
